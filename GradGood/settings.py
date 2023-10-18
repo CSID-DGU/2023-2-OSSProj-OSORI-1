1d93c163cf8079fabb26bad51724b61d74fbd599
JangMin99 <minizang99@dgu.ac.kr>
2023-10-18 10:58:47 +0900

    for test

--- GradGood/settings.py
+++ GradGood/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'GradGood.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': 3306,
     }
 }
 
