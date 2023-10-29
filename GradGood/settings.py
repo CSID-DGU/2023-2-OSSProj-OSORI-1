429acfc1f96bca2d1613eb396b3a2fb048ca54d5
JangMin99 <minizang99@dgu.ac.kr>
2023-10-29 23:27:33 +0900

    test

--- GradGood/settings.py
+++ GradGood/settings.py
@@ -14,7 +14,7 @@ import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # SECURITY WARNING: keep the secret key used in production secret!
