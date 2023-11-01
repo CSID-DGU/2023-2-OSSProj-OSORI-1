5fb6b77652165cd271050f89774fd6ed8390471e
JangMin99 <minizang99@dgu.ac.kr>
2023-11-01 13:16:07 +0900

    [1101 장민] add_login

--- GradGood/urls.py
+++ GradGood/urls.py
@@ -20,5 +20,6 @@ from main import views as main_views
 
 urlpatterns = [
     path('', main_views.index, name="index"),
+    path('', main_views.login, name="login"),
     path('admin/', admin.site.urls),
 ]
