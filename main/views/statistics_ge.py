590f81b5d4375ad83474b411c23ddfb248edd568
MeMe4834 <2021110472@dgu.ac.kr>
2023-11-08 15:17:00 +0900

    [1108 김미소]fix_꿀교양 추천목록

--- main/views/statistics_ge.py
+++ main/views/statistics_ge.py
@@ -14,6 +14,7 @@ def a_statistics(request):
     selection_list = request.POST.getlist('selection[]') # 선택영역 
     
     # 선택영역 동국대 강의형식으로 수정완료
+    selection =[]
     if "공교" in selection_list:
         selection += ["자아성찰","대학탐구","시민","글쓰기","명작","리더십","지역연구","영어","한국문화","SW"]
     if "일교" in selection_list:
@@ -26,29 +27,17 @@ def a_statistics(request):
         classification = ['공교', '일교', '학기'], 
         classification_ge = selection, 
         grade__in= grade_list
-    )
-    cs_count = cs_queryset.values_list('sum_stu').annotate(count=Count('sum_stu'))
-    # 쿼리셋을 리스트로 변환 -> 에브리타임 담은 강좌인원에 따라 내림차순 정렬 
-    
-    cs_count = sorted(list(cs_count), key = lambda x : x[1], reverse=True)
+    ).order_by('-sum_stu') # 에브리타임 담은 강좌인원에 따라 내림차순 정렬 
+
     zip_lecture_count = []
-    for s_num, count in cs_count:
-        if count < 10:
+    for lecture in cs_queryset:
+        if lecture.sum_stu < 10:
             continue
-        al_queryset = Lecture.objects.filter(
-            subject_num = s_num, 
-            subject_name = s_name,
-            classification = ['공교','학기','일교'], # 꿀교양찾기 페이지이기 때문에 교양만 추출 
-            professor =pro,
-            subject_credit = sub_c,
-            classification_ge =selection_list
-            grade__in=grade_list
-        )
-        if al_queryset.exists():
-            lec_info = list(al_queryset.values())[0]
-            zip_lecture_count.append([lec_info, count])
+        # 다른 필드에 관한 조건은 필요한 경우 추가할 것
+        zip_lecture_count.append([lecture])
+
     # context 전송
-    context={
+    context = {
         'zip_lecture_count': zip_lecture_count
     }
     return JsonResponse(context)
