ed2ccdb40ae13a4741e897431754f947484eead3
JangMin99 <minizang99@dgu.ac.kr>
2023-11-24 23:34:03 +0900

    [1124_장민] fix_총학점 삭제

--- main/models.py
+++ main/models.py
@@ -20,7 +20,6 @@ class UserInfo(models.Model):
     name = models.CharField(max_length=45) # 학생 이름
     thesis = models.CharField(max_length=10) # 졸업논문(시험) 통과여부(o,x)
     eng = models.CharField(max_length=30) # 영어 공인 성적(토익기준)
-    sum_credit = models.IntegerField() # 총 졸업학점
     email = models.CharField(max_length=30) # 비밀번호 찾기 기능 때 필요
     mypage_json = models.JSONField(blank=True, null=True)
     result_json = models.JSONField(blank=True, null=True)
