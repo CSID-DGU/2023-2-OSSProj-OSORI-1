75ea3a3f9a8456c35f78f9e74cfbec9a0c589895
MeMe4834 <2021110472@dgu.ac.kr>
2023-12-01 23:38:32 +0900

    [1201 김미소]fix_model

--- main/models.py
+++ main/models.py
@@ -39,7 +39,7 @@ class UserLecture(models.Model): # 학생성적정보파일 업로드 시 저장
     professor = models.CharField(max_length=50)
     student_id = models.CharField(max_length=10)
     major = models.CharField(max_length=50)
-    year = models.IntegerField()
+    year = models.CharField(max_length=10)
     semester = models.CharField(max_length=20)
     subject_credit = models.FloatField()
     id = models.AutoField(primary_key=True)
