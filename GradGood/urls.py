8c852d84e9b0a428de537de0e2d0402997a01ed9
MeMe4834 <2021110472@dgu.ac.kr>
2023-12-06 15:13:47 +0900

    [1206 김미소]add_admin_url

--- GradGood/urls.py
+++ GradGood/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('a_statistics/', views.a_statistics),
     path('a_statistics_ge/',views.a_statistics_ge),
     path('login/', views.r_login),
-    path('login_admin/',  views.r_login),
+    path('login_admin/',  views.r_login1),
     path('changePW/', views.r_changePW),
     path('mypage/', views.r_mypage),
     path('custom/', views.r_custom),
@@ -41,7 +41,7 @@ urlpatterns = [
 
      # 다른 함수사용 url 패턴
     path('f_login/', views.f_login),
-    path('f_login/', views.f_login1),
+    path('f_login1/', views.f_login1),
     path('f_logout/', views.f_logout),
     path('f_mypage/', views.f_mypage),
     path('f_register/', views.f_register),
