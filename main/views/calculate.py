c33fcc31ce1aa7b41007c9d61a1ae60317e45361
JangMin99 <minizang99@dgu.ac.kr>
2023-12-09 22:55:25 +0900

    [1209_장민] fix_전선필

--- main/views/calculate.py
+++ main/views/calculate.py
@@ -148,6 +148,7 @@ def f_result(user_id):
         standard_essential_selection = to_zip_list(list_to_query(dic_selection.keys()), check_selection)
     else:
         pass_ms_essential = 1
+        pass_sel_ess = 0
         standard_essential_selection = ['해당없음']
         
     if standard_row.eng_major <= major_eng:
@@ -166,7 +167,7 @@ def f_result(user_id):
     context_major_selection = {
         'standard_num' : standard_num_ms,
         'user_num' : convert_to_int(user_num_ms),
-        'pass_ess' : pass_ms_essential,
+        'pass_ess' : pass_sel_ess,
         'standard_essential' : standard_essential_selection,
         'english_essential' : major_eng_ess,
         'lack' : convert_to_int(lack_ms),
